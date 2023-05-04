51ce4256dcaeb8a89f2d460940a934c9e0b55a01
israel-09 <israelinene91@gmail.com>
2023-05-04 05:04:43 +0000

    task 8 solution

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+string = ARGV[0]
+from = ARGV[0].scan(/from:[0-9a-zA-Z+]+/)
+sender = from[0].scan(/[A-Z0-9+][A-Za-z0-9]+/)
+to = ARGV[0].scan(/to:[0-9a-zA-Z+]+/)
+receiver = to[0].scan(/[A-Z0-9+][A-Za-z0-9]+/)
+flag = ARGV[0].scan(/flags:[\-][0-9\-:]+/)
+log_flag = flag[0].scan(/[\-][0-9\-\:]+/)
+puts "#{sender[0]},#{receiver[0]},#{log_flag[0]}"
