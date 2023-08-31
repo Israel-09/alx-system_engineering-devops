d87bd4df301bc468dd0afde563e914788b701f89
israel-09 <israelinene91@gmail.com>
2023-08-31 01:18:39 +0000

    solution to task 7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,17 @@
+# script that installs and configure nginx for a web server
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { 'root page':
+  ensure => 'present',
+  path => '/var/www/html/index.html',
+  content => 'Hello World!',
+}
+
+file_line { 'configure redirect':
+  path => '/etc/nginx/sites-enabled/default',
+  line => "server_name _;\n\trewrite ^redirect_me https:\/\/www.youtube.com/watch?v=QH2-TGUlwu4 permanent;",
+  match => 'server_name _;',
+}
