c331e4f8dd48a2ae2c85605493a37fb9adee90b0
israel-09 <israelinene91@gmail.com>
2023-10-11 09:09:28 +0000

    upload solution to task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,6 +2,7 @@
 '''
 retrives information from reddit api
 '''
+
 import requests
 
 
@@ -17,14 +18,13 @@ def top_ten(subreddit):
 
     headers = {'User-Agent': "Microsoft edge 117.0.2045.47"}
     params = {'limit': 10}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     response = requests.get(url, headers=headers, params=params)
 
     try:
         data = response.json().get("data")
-        posts = data.get("children")[1:]
+        posts = data.get("children")
         for post in posts:
-            post_data = post.get('data')
-            print("{}".format(post_data.get('title')))
+            print(post.get('data').get('title'))
     except Exception:
         print("None")
