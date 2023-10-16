928f54d23dd42bdb6b87f4dd486cb5b1f2c8d468
israel-09 <israelinene91@gmail.com>
2023-10-16 21:13:08 +0000

    fixed typo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 
 exec { 'fix-server-error':
   provider => 'shell',
-  command  => "sed -i 's/.phpp/php/g' /var/www/html/wp-settings.php",
+  command  => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
 }
