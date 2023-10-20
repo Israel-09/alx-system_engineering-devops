6e15b7e22064a1076aef76f7553aca1a6fa42b4e
israel-09 <israelinene91@gmail.com>
2023-10-20 03:52:54 +0000

    task 1 solution

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# a manifest that enables the user holberton to login and open files without error
+ 
+
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  provider => 'shell',
+}
+
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command  => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  provider => 'shell',
+}
