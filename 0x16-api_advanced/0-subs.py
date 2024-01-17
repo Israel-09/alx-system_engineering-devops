4ca024c294f3e9748520c2812014e1a0e73a4dd6
israel-09 <israelinene91@gmail.com>
2024-01-17 22:12:51 +0100

    task 1 upload

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,15 +16,14 @@ def number_of_subscribers(subreddit):
     headers = {
                 "User-Agent": "python-requests/2.31.0"
               }
-    url = "https://www.reddit.com/r/{}.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers)
     try:
         if response.status_code != 200:
             return 0
         else:
             json_body = response.json()
-            child = json_body.get('data').get("children")[0]
-            no_of_subs = child.get('data').get("subreddit_subscribers")
+            no_of_subs = json_body.get('data').get("subscribers")
             return (no_of_subs)
     except Exception as e:
         return 0
